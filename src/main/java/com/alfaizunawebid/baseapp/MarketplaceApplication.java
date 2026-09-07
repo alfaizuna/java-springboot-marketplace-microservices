5cb336b2656c8e8399370e6c51246255af858c4d
alfaizuna <alfaizunaulia@gmail.com>
2026-09-07 15:19:15 +0700

    refactor: rename BaseappApplication classes to MarketplaceApplication for consistent project naming

--- src/main/java/com/alfaizunawebid/baseapp/MarketplaceApplication.java
+++ src/main/java/com/alfaizunawebid/baseapp/MarketplaceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaseappApplication {
+public class MarketplaceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BaseappApplication.class, args);
+		SpringApplication.run(MarketplaceApplication.class, args);
 	}
 
 }
--- src/test/java/com/alfaizunawebid/baseapp/MarketplaceApplicationTests.java
+++ src/test/java/com/alfaizunawebid/baseapp/MarketplaceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BaseappApplicationTests {
+class MarketplaceApplicationTests {
 
 	@Test
 	void contextLoads() {
