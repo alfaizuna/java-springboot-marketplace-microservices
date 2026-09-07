bb6df84a207734e0438c32bab9aa0170c7ba667f
alfaizuna <alfaizunaulia@gmail.com>
2026-09-07 18:48:06 +0700

    refactor: remove redundant @Repository annotations from JpaRepository interfaces

--- order-service/src/main/java/com/alfaizunawebid/order/repository/OrderRepository.java
+++ order-service/src/main/java/com/alfaizunawebid/order/repository/OrderRepository.java
@@ -3,11 +3,9 @@ package com.alfaizunawebid.order.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.alfaizunawebid.order.model.Order;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Optional<Order> findByOrderNumber(String orderNumber);
 }
--- payment-webhook-service/src/main/java/com/alfaizunawebid/payment/repository/PaymentTransactionLogRepository.java
+++ payment-webhook-service/src/main/java/com/alfaizunawebid/payment/repository/PaymentTransactionLogRepository.java
@@ -3,11 +3,9 @@ package com.alfaizunawebid.payment.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.alfaizunawebid.payment.model.PaymentTransactionLog;
 
-@Repository
 public interface PaymentTransactionLogRepository extends JpaRepository<PaymentTransactionLog, Long> {
     Optional<PaymentTransactionLog> findByTransactionId(String transactionId);
 }
